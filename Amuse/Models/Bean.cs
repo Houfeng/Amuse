499213b0afbfba56c281a026f619e77b8e88b16d
houfeng <admin@xhou.net>
2014-08-11 16:43:13 +0800

    dev

---
+++ Amuse/Models/Bean.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Amuse.Models
+{
+    internal class Bean
+    {
+        public Bean()
+        {
+            this.Properties = new List<Property>();
+            this.Methods = new List<Method>();
+        }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Type { get; set; }
+
+        public virtual string Mode { get; set; }
+
+        public virtual string Group { get; set; }
+
+        public virtual List<Property> Properties { get; set; }
+
+        public virtual List<Method> Methods { get; set; }
+
+        public virtual Constructor Constructor { get; set; }
+
+        public virtual string FactoryMethod { get; set; }
+    }
+}
---
+++ Amuse/Models/Property.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Amuse.Models
+{
+    internal class Property
+    {
+        public Property(Bean bean)
+        {
+            this.Bean = bean;
+        }
+        public virtual Bean Bean { private set; get; }
+        public virtual string Name { get; set; }
+        public virtual string Ref { get; set; }
+        public virtual string Value { get; set; }
+        public virtual string Type { get; set; }
+    }
+}
